7dd4912387a47da11edc88347ed171c926dd597a
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-07 14:47:29 +0900

    Navbar and Profile

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
 
 export default function Home() {
   return (
@@ -11,10 +16,60 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className="bg-blue-700 min-h-screen">
+      <main className='bh-white px-10'>
+      {/* Top Section Start */}
+      <section className="min-h-screen">
+      <nav className="p-10 mb-12 flex justify-between">
+        <h1 className='text-xl font-burtons'>smackdh</h1>
+        <ul className='flex items-center'>
+          <li>
+            <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
+          </li>
+          <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 border-none" href="#">Resume</a></li>
+        </ul>
+        </nav>
+        {/* Profile Info */}
+        <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium'>Mattias Velamsson</h2>
+            <h3 className='text-2xl py-2'>Fullstack Developer</h3>
+            <p className='text-md py-5 leading-6 text-gray-800'>
+              Hey, I'm Mattias!
+              I used to work as a technical engineer and team leader back in Sweden for 7 years. I've experimented with code, databases and more since a young age and always loved it. Coming to Japan I decided I wanted to take the opportunity and get back to my childhood hobbies and become a developer. I graduated from LeWagon bootcamp during winter 2022 and am now looking forward to new challenges in my career-path!
+              Hit me up on LinkedIn or by mail if you'd like to know more about me or have any questions!
+            </p>
 
-        </section>
+        </div>
+        {/* Links to socials */}
+        <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillGithub/>
+            <AiFillLinkedin/>
+        </div>
+        <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+        <Image src={deved} layout="fill" objectFit="cover"/>
+        </div>
+      </section>
+      {/* Top Section End */}
+
+      <section>
+      <div>
+        <h3 className="text-3xl py-1">Services I offer</h3>
+        <p className='text-md py-2 leading-6 text-gray-80'>What I've been working on.....
+        <span className='text-teal-500'> Ozei </span>
+        a group project of 4 people, creating and deploying a working Prototype in 2 weeks.
+        </p>
+        <p className='text-md py-2 leading-6 text-gray-80'>This is some more informational text....
+        <span className='text-teal-500'> Subject </span>
+        Put something interesting and important here..
+        </p>
+      </div>
+      <div>
+        <div>
+          <Image src={design} width={100} height={100}/>
+          <h3>Beautiful Designs</h3>
+          <p>I make some sick ass designs.</p>
+        </div>
+      </div>
+      </section>
       </main>
     </div>
   );
