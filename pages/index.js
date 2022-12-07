549587b675136ccb73cd3811957690599163205d
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-07 15:49:48 +0900

    imported useState

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,11 @@ import web3 from '../public/web3.png';
 import web4 from '../public/web4.png';
 import web5 from '../public/web5.png';
 import web6 from '../public/web6.png';
+import {useState} from "react";
 
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <div>
       <Head>
